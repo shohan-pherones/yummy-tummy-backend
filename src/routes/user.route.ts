6b98f9642be2eb9db8189d87a09a8d7d630537c6
shohan-pherones <shohan.sub.56@gmail.com>
2024-03-22 00:18:05 +0600

    jwt check

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
 import { createCurrentUser } from "../controllers/user.controller";
+import { jwtCheck } from "../middlewares/auth.middleware";
 
 const router: Router = express.Router();
 
-router.post("/", createCurrentUser);
+router.post("/", jwtCheck, createCurrentUser);
 
 export default router;
