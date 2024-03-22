acb210f3dc63b6978e9ee78cce5ad2b3ac060c4c
shohan-pherones <shohan.sub.56@gmail.com>
2024-03-22 23:33:43 +0600

    update user api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -19,3 +19,26 @@ export const createCurrentUser = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Error creating user" });
   }
 };
+
+export const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+
+    await user.save();
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,14 @@
 import express, { Router } from "express";
-import { createCurrentUser } from "../controllers/user.controller";
-import { jwtCheck } from "../middlewares/auth.middleware";
+import {
+  createCurrentUser,
+  updateCurrentUser,
+} from "../controllers/user.controller";
+import { jwtCheck, jwtParse } from "../middlewares/auth.middleware";
+import { validateMyUserRequest } from "../dto/user.dto";
 
 const router: Router = express.Router();
 
 router.post("/", jwtCheck, createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, updateCurrentUser);
 
 export default router;
