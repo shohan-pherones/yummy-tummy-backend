4788e7477604a9f5ed0f787d84bd61a203d902f6
shohan-pherones <shohan.sub.56@gmail.com>
2024-03-24 22:46:51 +0600

    ready for production

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 import "dotenv/config";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { connectDb } from "./config/db.config";
 import myUserRoute from "./routes/user.route";
 
@@ -8,6 +8,10 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.status(200).json({ message: "OK!" });
+});
 
 app.use("/api/my/user", myUserRoute);
 
