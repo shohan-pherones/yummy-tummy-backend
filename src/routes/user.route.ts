2b77cecd1db6fdd26547f86013a3a11b437b50fb
shohan-pherones <shohan.sub.56@gmail.com>
2024-03-24 23:23:22 +0600

    get user api

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import {
   createCurrentUser,
+  getCurrentUser,
   updateCurrentUser,
 } from "../controllers/user.controller";
 import { jwtCheck, jwtParse } from "../middlewares/auth.middleware";
@@ -8,6 +9,7 @@ import { validateMyUserRequest } from "../dto/user.dto";
 
 const router: Router = express.Router();
 
+router.get("/", jwtCheck, jwtParse, getCurrentUser);
 router.post("/", jwtCheck, createCurrentUser);
 router.put("/", jwtCheck, jwtParse, validateMyUserRequest, updateCurrentUser);
 
