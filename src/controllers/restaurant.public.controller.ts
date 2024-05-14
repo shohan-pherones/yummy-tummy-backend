bb31949321ca6d051cc822126474c7700d701002
shohan-pherones <shohan.sub.56@gmail.com>
2024-05-14 22:00:11 +0600

    get public restaurant by id

--- src/controllers/restaurant.public.controller.ts
+++ src/controllers/restaurant.public.controller.ts
@@ -66,3 +66,20 @@ export const searchRestaurant = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+export const getPublicRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurantId = req.params.restaurantId;
+
+    const restaurant = await Restaurant.findById(restaurantId);
+
+    if (!restaurant) {
+      return res.status(404).json({ messsge: "Restaurant not found" });
+    }
+
+    res.status(200).json(restaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
--- src/routes/restaurant.public.route.ts
+++ src/routes/restaurant.public.route.ts
@@ -1,8 +1,21 @@
 import express, { Router } from "express";
 import { param } from "express-validator";
-import { searchRestaurant } from "../controllers/restaurant.public.controller";
+import {
+  getPublicRestaurant,
+  searchRestaurant,
+} from "../controllers/restaurant.public.controller";
 
 const router: Router = express.Router();
+
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("Restaurant ID parameter must be a valid string"),
+  getPublicRestaurant
+);
 
 router.get(
   "/search/:city",
